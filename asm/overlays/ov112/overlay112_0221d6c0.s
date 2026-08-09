808983eab98f40a8b69ccd38419127833fa3f1d7
Tim Stadler <timstadler905@googlemail.com>
2026-08-09 16:25:46 +0200

    Complete overlay 112 decompilation

---
+++ asm/overlays/ov112/overlay112_0221d6c0.s
@@ -0,0 +1,36 @@
+.text
+; Matching fallback for the portable implementation in src/overlays/ov112/overlay112_recovery.c.
+.extern data_ov112_0221d72c
+.extern data_ov112_0221d740
+.extern data_ov112_0221e67d
+.extern data_ov112_0221e7a0
+.extern data_ov112_0221e7a4
+.extern func_02008f58
+.extern func_0203aac4
+.extern func_0203abac
+.extern func_0203b38c
+.extern func_0203b3ac
+
+.global func_ov112_0221d6c0
+func_ov112_0221d6c0:
+    stmdb sp!, {r3, lr}
+    cmp r0, #0x0
+    ldmneia sp!, {r3, pc}
+    bl func_0203aac4
+    ldr r0, .L_0221d6fc
+    ldr r2, .L_0221d700
+    mov r1, #0x3
+    bl func_0203b38c
+    ldr r0, .L_0221d704
+    bl func_0203abac
+    ldr r0, .L_0221d708
+    bl func_02008f58
+    ldr r0, .L_0221d70c
+    bl func_0203b3ac
+    ldmia sp!, {r3, pc}
+.L_0221d6fc: .word data_ov112_0221e7a4
+.L_0221d700: .word data_ov112_0221d740
+.L_0221d704: .word data_ov112_0221e7a0
+.L_0221d708: .word data_ov112_0221d72c
+.L_0221d70c: .word data_ov112_0221e67d
+.size func_ov112_0221d6c0, . - func_ov112_0221d6c0
